43e3b30c1d40d6216b320d1e28d45c5740b092c4
NotDewey <mor3no.garcia.david@gmail.com>
2025-09-01 22:15:42 -0600

    Initial Commit

---
+++ AvanceProyecto/AvanceProyectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvanceProyectoApp.swift
+//  AvanceProyecto
+//
+//  Created by David Moreno on 2025-09-01.
+//
+
+import SwiftUI
+
+@main
+struct AvanceProyectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
