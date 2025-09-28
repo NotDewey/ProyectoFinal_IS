857fa5aaae9ac3b96eb171124e7325aa4f101878
NotDewey <mor3no.garcia.david@gmail.com>
2025-09-28 06:20:08 -0600

    ci: add CI/CD pipeline with Codecov integration

---
+++ AvanceProyectoTests/DonorViewModelTests.swift
@@ -0,0 +1,8 @@
+//
+//  DonorViewModelTests.swift
+//  AvanceProyectoTests
+//
+//  Created by David Moreno on 2025-09-28.
+//
+
+import Foundation
---
+++ AvanceProyectoTests/MockDonorService.swift
@@ -0,0 +1,8 @@
+//
+//  MockDonorService.swift
+//  AvanceProyectoTests
+//
+//  Created by David Moreno on 2025-09-28.
+//
+
+import Foundation
